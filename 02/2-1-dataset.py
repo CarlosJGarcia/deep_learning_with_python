706cb90b1833acfdd63bd3e4741cf422be600867
CarlosJGarcia <cgarciams@hotmail.com>
2025-12-27 06:42:44 +0100

    capitulo 3

--- 02/2-1-dataset.py
+++ 02/2-1-dataset.py
@@ -1,4 +1,5 @@
 from keras.datasets import mnist
+import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
@@ -8,11 +9,6 @@ print(f"Clase:{type(train_labels)}")
 print(f"Dimensiones: {train_labels.ndim}")
 print(f"Shape: {train_labels.shape}")
 print(f"Clase de los items: {train_labels.dtype}")
-"""
-print(f"Primeros 5 valores: {train_labels[:5]}")
-for n in range(5):
-    print(n, train_labels[n])
-"""
     
 print()
 print("train_images:")
@@ -21,5 +17,12 @@ print(f"Dimensiones: {train_images.ndim}")
 print(f"Shape: {train_images.shape}")
 print(f"Clase de los items: {train_images.dtype}")
 
-print("Fin del programa.")
 print()
+print(f"Primeros 5 valores: {train_labels[:5]}")
+for n in range(5):
+    print(n, train_labels[n])
+plt.imshow(train_images[0], cmap=plt.cm.binary)
+plt.show()
+
+print()
+print("Fin del programa.")
--- 02/2-1.py
+++ 02/2-1.py
@@ -1,10 +1,19 @@
+# 2-1.py
+# Clasificador de dígitos escritos a mano MNIST, implementado en Keras
+# Carlos Garcia, Zaragoza Diciembre 2025
+
 from keras import models
 from keras import Input
 from keras import layers
 from keras.datasets import mnist
+from keras.utils import to_categorical
 
+print()
+print("1. Carga de datos")
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+print()
+print("2. Deficición del modelo y preparación de los datos")
 # Sequential model, the simplest type of Keras model
 network = models.Sequential()
 
@@ -22,7 +31,7 @@ network.add(layers.Dense(512, activation='relu'))
 # As this is the last layer, it will be the output layer
 network.add(layers.Dense(10, activation='softmax'))
 
-network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+network.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # De array 3D (60000, 28, 28) a 2D (60000, 28*28). 28*28=786 es el número de neuronas en la capa de entrada.
 # Typecasting de entero 8 bits a punto flotante 32 bits. Es un requerimiento del algoritmo de backpropagation
@@ -32,4 +41,21 @@ train_images = train_images.astype('float32')/255
 test_images = test_images.reshape(10000, 28*28)
 test_images = test_images.astype('float32')/255
 
+# Training
+print()
+print("3. Model training")
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+# Evaluación
+print()
+print("4. Evaluación con datos de test que el modelo no ha visto")
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+
+print("======================================================")
+print(f"Loss con datos de test: {test_loss}")
+accu = test_acc * 100
+print(f"Accuracy con datos de test: {test_acc} - {accu:.1f}%")
+
+# Fin
+print()
 print("Fin del programa.")
